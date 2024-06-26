b9324e551cecb770a1952ddd4c3f496215adb342
zhanglipeng008 <zhanglipeng008@ke.com>
2024-06-26 23:11:22 +0800

    v1_codeclean

--- repeat_cur_page/popups/popup.js
+++ repeat_cur_page/popups/popup.js
@@ -1,27 +1,3 @@
-// document.getElementById('myButton').addEventListener('click', function () {
-//   // chrome.runtime.sendMessage({ action: "buttonClicked" }, function (response) {
-//   //   console.log("Received response from background:", response);
-//   // });
-
-//     // const urlToOpen = 'https://example.com'; // 将此URL替换为你想打开的页面地址
-//     // chrome.tabs.create({ url: urlToOpen });
-
-//   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-//     var currentTab = tabs[0];
-//     var currentUrl = currentTab.url;
-
-//     // 在新标签页中打开当前页面的URL
-//     chrome.tabs.create({ url: currentUrl });
-//   })
-
-
-// });
-
-// // 与后台脚本通信的示例
-// chrome.runtime.sendMessage({ action: "popupOpened" }, response => {
-//   console.log("Received response from background:", response);
-// });
-
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
   var currentTab = tabs[0];
   var currentUrl = currentTab.url;
